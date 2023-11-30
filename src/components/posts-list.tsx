ff850608b849aaf174ae695f4ee7d9393cee37a6
Albert Dang <98789245+albertdang8@users.noreply.github.com>
2023-11-29 17:19:36 -0800

    card tests

--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -8,18 +8,30 @@ export default async function PostsList() {
 
   return (
     <section className="bg-[#1c5d992d] p-4 border">
-    <h1 className="text-3xl italic">Featured Posts:</h1>
+      <h1 className="text-3xl italic">Featured Posts:</h1>
       <ul className="flex flex-wrap gap-14 justify-around py-12">
         {data.posts.map((post: any) => (
           <li key={post.id} className="flex flex-col items-center">
-            <Image src="/blogRef.png" alt="test" width={250} height={100} className="rounded-sm mb-4" />
+            <div className="">
+              <Image
+                src="/p3.jpg"
+                alt="test"
+                width={250}
+                height={250}
+                className="h-[200px] rounded-sm mb-4 object-cover"
+              />
+            </div>
             <div>
-              <h2 className="max-w-[250px] font-bold capitalize text-2xl pb-8 border-b">{post.title}</h2>
+              <h2 className="max-w-[250px] font-bold capitalize text-2xl pb-8 border-b">
+                {post.title}
+              </h2>
               <h4 className="pt-2">
                 Tags:{" "}
                 {post.tags.map((tag: string, index: number) => (
                   <span key={index}>
-                    <span className="cursor-pointer hover:text-accentLight transition-colors">#{tag}</span>
+                    <span className="cursor-pointer hover:text-accentLight transition-colors">
+                      #{tag}
+                    </span>
                     {index !== post.tags.length - 1 ? ", " : ""}
                   </span>
                 ))}
